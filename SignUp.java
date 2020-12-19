ad5b98254bd6fb04295a61448f0d7e5832e3135f
Aybüke FIRAT <66526972+aybukefirat@users.noreply.github.com>
2020-12-19 12:42:31 +0300

    Add files via upload

---
+++ SignUp.java
@@ -0,0 +1,20 @@
+package com.aybukefirat.readzy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.lang.invoke.CallSite;
+
+public class SignUp<CallSignUp, callSignUp> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+
+}
